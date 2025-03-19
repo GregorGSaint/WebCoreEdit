99f79fda98fbd27d7acaa5a18aebc61f85b1a60d
GregorGSaint <dgjy6gbgdbt@gmail.com>
2025-03-19 16:53:21 +0300

    fgjfgj

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/js/index.js", 
-
+  entry: "./src/js/index.js", // Основной файл JS
   output: {
-    path: __dirname + "/dist",  
-    filename: "js/bundle.js"
+    path: path.resolve(__dirname, "dist"), // Куда собираем файлы
+    filename: "js/bundle.js",
+    clean: true, // Очищает папку dist перед сборкой
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html", 
-      filename: "index.html" 
+      template: "./src/index.html", // Базовый HTML
+      filename: "index.html", // Копируем в dist
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css", // Стили попадают в dist
     }),
   ],
 
@@ -20,11 +25,20 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          "style-loader", 
-          "css-loader",   
-          "sass-loader"   
-        ]
+          MiniCssExtractPlugin.loader, // Генерируем CSS-файл
+          "css-loader",
+          "sass-loader",
+        ],
       },
-    ]
-  }
+    ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"), // Отдаем файлы из dist
+    },
+    port: 3000,
+    open: true, // Автоматически открывать браузер
+    historyApiFallback: true,
+  },
 };
