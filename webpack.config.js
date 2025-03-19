391cb4936f95eacb492926ae7a7043fb891b2d2b
GregorGSaint <dgjy6gbgdbt@gmail.com>
2025-03-19 16:39:07 +0300

    fxfxfx

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  
-  entry: [
-    './src/js/index.js'
-  ],
+  entry: "./src/js/index.js", 
 
-  
   output: {
-    filename: './js/bundle.js'
+    path: __dirname + "/dist",  
+    filename: "js/bundle.js"
   },
 
- 
-  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", 
+      filename: "index.html" 
+    }),
+  ],
 
   module: {
     rules: [
-      
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          }
-        }
-      },
-
-      
       {
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader"
+          "style-loader", 
+          "css-loader",   
+          "sass-loader"   
         ]
-      }
-
-      
-    ],
-  },
-  plugins: [
-    
-    new HtmlWebpackPlugin({
-      title: 'Webpack 4 Starter',
-      template: './src/index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: false,
-      }
-    }),
-
-    
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-
-    
-    new CopyWebpackPlugin({
-                   patterns: [
-                     {
-                       from: './src/img',
-                       to: './img',
-                     },
-                     {
-                       from: './src/fonts',
-                       to: './fonts', 
-                     },
-                   ],
-                 }),
-  ],
+      },
+    ]
+  }
 };
